cb6139d5293ba94fc2ab029c60a4e202ec81ef3b
candace <candace.marzec@gmail.com>
2019-05-13 15:46:37 -0700

    adds value to squares

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => {
+        alert('click'); }}>
+        {this.props.value}
       </button>
     );
   }
@@ -15,7 +23,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
